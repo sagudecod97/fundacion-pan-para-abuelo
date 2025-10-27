a4ce8b0e3dd229829aec581abad3cda576d494b1
Santiago A <sagudecod97@gmail.com>
2025-10-27 17:52:16 -0500

    Added hover event. Keyboard controls. Will change main content presentation and tablist content props

--- src/scripts/lib/carousel-class.js
+++ src/scripts/lib/carousel-class.js
@@ -5,28 +5,40 @@ export class Carousel {
     this.carouselId = carouseldId;
     this.initialRender = true;
     this.mainContainer;
+    this.carouselContent;
+    this.tabPanel;
     this.carouselImage;
     this.toggleRotationBtn;
     this.toggleRotationIcon;
     this.tabsContainer;
     this.tabsElements;
+
+    // Bindings
     this.toggleCarouselRotationHandler =
       this.toggleCarouselRotationHandler.bind(this);
     this.clickOnTabHandler = this.clickOnTabHandler.bind(this);
     this.keyboardControlsHandler = this.keyboardControlsHandler.bind(this);
+    this.stopCarouselRotationHandler =
+      this.stopCarouselRotationHandler.bind(this);
+    this.carouselRotationHandler = this.carouselRotationHandler.bind(this);
 
+    // Select carousel elements
     this.getCarouselElements();
 
+    // Setting event listeners
     this.carouselRotationHandler();
     this.setElementsOnFocusHandler();
     this.settoggleCarouselRotationHandler();
     this.setClickOnTabHandler();
     this.setKeyboardControlsHandler();
+    this.setHoveringCarouselHandler();
   }
 
   // Get carousel elements
   getCarouselElements() {
     this.mainContainer = query(`#${this.carouselId}`);
+    this.carouselContent = query(".carousel__content", this.mainContainer);
+    this.tabPanel = query(".carousel__content-tabpanel", this.mainContainer);
     this.toggleRotationBtn = query(".carousel__rotation", this.mainContainer);
     this.toggleRotationIcon = query("use", this.toggleRotationBtn);
     this.carouselImage = query(".carousel__content-img", this.mainContainer);
@@ -64,6 +76,10 @@ export class Carousel {
       // Toggles aria-selected values.
       activeTab.setAttribute("aria-selected", "false");
       nextActiveTab.setAttribute("aria-selected", "true");
+      this.tabPanel.setAttribute(
+        "aria-label",
+        `${this.tabsElements.indexOf(nextActiveTab) + 1} de 4`
+      );
 
       // Removes and adds active class. Defines main container background image
       activeTab.classList.remove("--active");
@@ -76,6 +92,15 @@ export class Carousel {
   }
 
   stopCarouselRotationHandler() {
+    this.toggleRotationIcon.setAttribute(
+      "href",
+      "/src/assets/icons/icons-sprite.svg#player-play"
+    );
+    this.toggleRotationBtn.setAttribute(
+      "aria-label",
+      "Iniciar rotación automatica"
+    );
+    this.carouselContent.setAttribute("aria-live", "polite");
     clearInterval(window[`_${this.carouselId}`]);
     delete window[`_${this.carouselId}`];
   }
@@ -83,16 +108,16 @@ export class Carousel {
   toggleCarouselRotationHandler() {
     if (!window[`_${this.carouselId}`]) {
       this.carouselRotationHandler();
+      this.toggleRotationBtn.setAttribute(
+        "aria-label",
+        "Detener rotación automatica"
+      );
       this.toggleRotationIcon.setAttribute(
         "href",
         "/src/assets/icons/icons-sprite.svg#player-pause"
       );
     } else {
       this.stopCarouselRotationHandler(`_${this.carouselId}`);
-      this.toggleRotationIcon.setAttribute(
-        "href",
-        "/src/assets/icons/icons-sprite.svg#player-play"
-      );
     }
   }
 
@@ -110,46 +135,65 @@ export class Carousel {
     tabElement.setAttribute("aria-selected", "true");
     currentActiveTab.setAttribute("aria-selected", "false");
     this.carouselImage.src = imageElement.src;
+    this.tabPanel.setAttribute(
+      "aria-label",
+      `${this.tabsElements.indexOf(tabElement) + 1} de 4`
+    );
   }
 
   keyboardControlsHandler(event) {
-    event.preventDefault();
     const { key } = event;
 
-    if (!["ArrowRight", "ArrowLeft", "Home", "End"].includes(key)) return;
+    if (
+      [
+        "ArrowRight",
+        "ArrowLeft",
+        "ArrowUp",
+        "ArrowDown",
+        "Home",
+        "End",
+      ].includes(key)
+    ) {
+      event.preventDefault();
+      const activeTab = query(".--active", this.tabsContainer);
+      const indexActiveTab = this.tabsElements.indexOf(activeTab);
+      const tabsElementsLenght = this.tabsElements.length - 1;
+      let newActiveTab;
+      let newActiveTabPosition;
 
-    const activeTab = query(".--active", this.tabsContainer);
-    const indexActiveTab = this.tabsElements.indexOf(activeTab);
-    const tabsElementsLenght = this.tabsElements.length - 1;
-    let newActiveTab;
-    let newActiveTabPosition;
+      if (key === "ArrowRight") {
+        newActiveTabPosition =
+          indexActiveTab === tabsElementsLenght ? 0 : indexActiveTab + 1;
+      } else if (key === "ArrowLeft") {
+        newActiveTabPosition =
+          indexActiveTab === 0 ? tabsElementsLenght : indexActiveTab - 1;
+      } else if (key === "Home") {
+        newActiveTabPosition = 0;
+      } else if (key === "End") {
+        newActiveTabPosition = tabsElementsLenght;
+      } else {
+        return;
+      }
 
-    if (key === "ArrowRight") {
-      newActiveTabPosition =
-        indexActiveTab === tabsElementsLenght ? 0 : indexActiveTab + 1;
-    } else if (key === "ArrowLeft") {
-      newActiveTabPosition =
-        indexActiveTab === 0 ? tabsElementsLenght : indexActiveTab - 1;
-    } else if (key === "Home") {
-      newActiveTabPosition = 0;
-    } else if (key === "End") {
-      newActiveTabPosition = tabsElementsLenght;
+      newActiveTab = this.tabsElements[newActiveTabPosition];
+
+      if (newActiveTab === activeTab) return;
+
+      const newActiveImageElement = query("img", newActiveTab);
+
+      // Add/Remove --active class
+      newActiveTab.classList.add("--active");
+      activeTab.classList.remove("--active");
+
+      // Add/Remove aria-selected
+      newActiveTab.setAttribute("aria-selected", "true");
+      activeTab.setAttribute("aria-selected", "false");
+      this.tabPanel.setAttribute(
+        "aria-label",
+        `${this.tabsElements.indexOf(newActiveTab) + 1} de 4`
+      );
+      this.carouselImage.src = newActiveImageElement.src;
     }
-
-    newActiveTab = this.tabsElements[newActiveTabPosition];
-
-    if (newActiveTab === activeTab) return;
-
-    const newActiveImageElement = query("img", newActiveTab);
-
-    // Add/Remove --active class
-    newActiveTab.classList.add("--active");
-    activeTab.classList.remove("--active");
-
-    // Add/Remove aria-selected
-    newActiveTab.setAttribute("aria-selected", "true");
-    activeTab.setAttribute("aria-selected", "false");
-    this.carouselImage.src = newActiveImageElement.src;
   }
 
   // Set handlers
@@ -160,10 +204,6 @@ export class Carousel {
   setElementsOnFocusHandler = () => {
     const handleFocusEvent = () => {
       if (window[`_${this.carouselId}`]) {
-        this.toggleRotationIcon.setAttribute(
-          "href",
-          "/src/assets/icons/icons-sprite.svg#player-play"
-        );
         this.stopCarouselRotationHandler(`_${this.carouselId}`);
       }
     };
@@ -187,5 +227,19 @@ export class Carousel {
       "keydown",
       this.keyboardControlsHandler
     );
+  }
+
+  setHoveringCarouselHandler() {
+    this.mainContainer.addEventListener(
+      "mouseover",
+      this.stopCarouselRotationHandler
+    );
+    this.mainContainer.addEventListener("mouseout", () => {
+      this.carouselRotationHandler();
+      this.toggleRotationIcon.setAttribute(
+        "href",
+        "/src/assets/icons/icons-sprite.svg#player-pause"
+      );
+    });
   }
 }
